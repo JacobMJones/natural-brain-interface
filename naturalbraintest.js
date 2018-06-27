30887e3caefc4c93f7ef3cac22dacf51aaa9c0fd
JacobMJones <mrjacobtaiwa@gmail.com>
2018-06-27 07:38:25 -0400

    Brought work over from my original fork

---
+++ naturalbraintest.js
@@ -0,0 +1,146 @@
+var BrainJSClassifier = require("natural-brain");
+var classifier = new BrainJSClassifier();
+const fs = require('fs');
+
+const createEmptyJson = (name, content) => {
+    typeof name === 'string' ? fs.writeFileSync(`${name}.json`, `${content}`) : console.log('create file failed')
+}
+
+// saves classifier to filename
+// const saveClassifier = filename => {
+//   classifier.save(filename, function(err, classifier) {});
+// };
+const saveClassifier = filename => {
+        return new Promise((resolve, reject) => {
+            classifier.save(filename, (err, classifier) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                resolve(classifier)
+            })
+        })
+    }
+    // loads classifier from filename, then does callback
+const loadClassifier = (filename, cb) => {
+    BrainJSClassifier.load(filename, null, null, cb);
+};
+
+
+
+
+const loadedCB = (error, classifier) => {
+
+    console.log(classifier.classify("Wow so good"));
+    console.log(classifier.classify("That was bad food"));
+
+};
+loadClassifier('classifier.json', loadedCB)
+const retrainClassifier = (error, classifier) => {
+    console.log("retraining");
+    classifier.addDocument("i love pie", "food");
+    console.log("new feature added");
+    classifier.retrain();
+    saveClassifier('classifier.json')
+    return Promise.resolve('success');
+}
+
+const createNewClassifier = () => {
+    return new Promise((resolve, reject) => {
+        classifier.addDocument("my unit-tests failed.", "software");
+        // classifier.addDocument("tried the program, but it was buggy.", "software");
+        // classifier.addDocument("tomorrow we will do standup.", "meeting");
+        // classifier.addDocument("the drive has a 2TB capacity.", "hardware");
+        // classifier.addDocument("i need a new power supply.", "hardware");
+        // classifier.addDocument("can you play some new music?", "music");
+        classifier.train();
+        // console.log('classifier trained')
+        saveClassifier("classifier.json");
+        //console.log('classifier saved')
+        resolve()
+    })
+};
+
+
+const prepareDataSet = (dataSet) => {
+    //console.log('in prepareDataSet');
+    var parsedArray = fs.readFileSync(dataSet).toString().split("\n");;
+    finalArray = [];
+    for (var i = 0; i < 20; i++) {
+        let dataPoint = parsedArray[i];
+        let match = dataPoint.match(/(.*)\s(0|1)$/)
+        dataObject = {};
+        dataObject.content = match[1];
+        match[2] == 1 ? dataObject.sentiment = 'positive' : dataObject.sentiment = 'negative';
+        finalArray.push(dataObject);
+    }
+    //console.log(finalArray);
+    addDataSet(finalArray);
+}
+const addDataSet = (dataSet) => {
+
+    dataSet.forEach(function(item) {
+        classifier.addDocument(item.content, item.sentiment);
+    });
+    classifier.train();
+    saveClassifier('classifier.json');
+    // console.log(dataSet[0])
+
+}
+
+
+
+
+//prepareDataSet('yelp.txt');
+
+
+// const createNewClassifier = async() => {
+//     classifier.addDocument("my unit-tests failed.", "software");
+//     // classifier.addDocument("tried the program, but it was buggy.", "software");
+//     // classifier.addDocument("tomorrow we will do standup.", "meeting");
+//     // classifier.addDocument("the drive has a 2TB capacity.", "hardware");
+//     // classifier.addDocument("i need a new power supply.", "hardware");
+//     // classifier.addDocument("can you play some new music?", "music");
+//     classifier.train();
+//     console.log('classifier trained')
+//     saveClassifier("classifier.json");
+//     console.log('classifier saved')
+//     return
+// };
+
+const foo = () => {
+    createNewClassifier().then(loadCB)
+}
+
+// const foo = async() => {
+//     await createNewClassifier()
+//     loadCB
+// }
+
+
+createNewClassifier()
+    .then(r => { loadClassifier('classifier.json', loadedCB) })
+
+
+// .then(r => {loadClassifier('classifier.json', retrainClassifier)})
+// .then(r => {loadClassifier('classifier.json', loadedCB)})
+
+// createWhole
+// .then(saveClassifier('classifier.json'))
+// .then(r => {loadClassifier('classifier.json', loadedCB)})
+// .then(r => {loadClassifier('classifier.json', retrainClassifier)})
+// .then(r => {loadClassifier('classifier.json', loadedCB)})
+
+
+let createWhole = new Promise((resolve, reject) => {
+    classifier.addDocument("my unit-tests failed.", "software");
+    classifier.addDocument("tried the program, but it was buggy.", "software");
+    classifier.addDocument("tomorrow we will do standup.", "meeting");
+    classifier.addDocument("the drive has a 2TB capacity.", "hardware");
+    classifier.addDocument("i need a new power supply.", "hardware");
+    classifier.addDocument("can you play some new music?", "music");
+    classifier.addDocument("pie is my greatest love", "food");
+    classifier.train();
+    resolve('success')
+        //   // saveClassifier("classifier.json");
+})
---
+++ tester.js
@@ -0,0 +1,119 @@
+var BrainJSClassifier = require("natural-brain");
+var classifier = new BrainJSClassifier();
+const fs = require('fs');
+
+let stopWords = ['i', 'is', 'be', 'am', 'this', 'restaurant', 'are', 'was', 'will', 'were', 'than', 'that', 'then',
+    'there', 'ours', 'our', 'ought', 'other', 'or', 'only', 'once', 'on', 'off', 'myself', 'most', 'more', 'let', 'its',
+    'how', 'her', 'his', 'him', 'he', 'she', 'for', 'few', 'had', 'each', '.', 'being', 'could', 'should'
+];
+const createEmptyJson = (name, content) => {
+    typeof name === 'string' ? fs.writeFileSync(`${name}.json`, `${content}`) : console.log('create file failed')
+}
+
+const saveClassifier = filename => {
+    return new Promise((resolve, reject) => {
+        classifier.save(filename, (err, classifier) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(classifier)
+        })
+    })
+}
+
+const loadClassifier = (filename, cb) => {
+    BrainJSClassifier.load(filename, null, null, cb);
+};
+
+const testCB = (error, classifier) => {
+    console.log("The candy was bad\n", classifier.getClassifications("The candy was bad"));
+    console.log("The food was good\n", classifier.getClassifications("The food was good"));
+    console.log("The candy was not bad\n", classifier.getClassifications("The food was not good not bad"));
+};
+
+const addSingleDocument = (content, sentiment) => {
+    console.log('in add single', content, sentiment);
+    content = removeStopWords(content);
+    dataObject = {};
+    dataObject.content = content;
+    dataObject.sentiment = sentiment;
+    addDataSet([dataObject]);
+}
+const prepareDataSet = (dataSet) => {
+    var parsedArray = fs.readFileSync(dataSet).toString().split("\n");;
+    finalArray = [];
+    for (var i = 0; i < 70; i++) {
+        console.log(i);
+        let dataPoint = parsedArray[i];
+        let match = dataPoint.match(/(.*)\s(0|1)$/)
+        dataObject = {};
+
+        //match 1 has content and match 2 has sentiment
+        let content = match[1];
+        content = removeStopWords(content);
+        dataObject.content = content;
+
+        match[2] == 1 ? dataObject.sentiment = 'positive' : dataObject.sentiment = 'negative';
+        finalArray.push(dataObject);
+    }
+    addDataSet(finalArray);
+}
+
+const removeStopWords = (content) => {
+
+
+    return content.split(' ').filter(word => stopWords.indexOf(word.toLowerCase()) == -1).join(' ');
+}
+
+function is_in_array(s, your_array) {
+    for (var i = 0; i < your_array.length; i++) {
+        if (your_array[i].toLowerCase() === s.toLowerCase()) return true;
+    }
+    return false;
+}
+
+const retrainClassifier = (error, classifier) => {
+    console.log("retraining");
+    prepareDataSet('yelp.txt')
+    console.log("new feature added");
+
+
+    classifier.retrain();
+    saveClassifier('classifier.json')
+}
+const addDataSet = (dataSet) => {
+
+    console.time();
+    dataSet.forEach(function(item) {
+        classifier.addDocument(item.content, item.sentiment);
+    });
+    console.timeEnd();
+    console.time();
+    classifier.train();
+    console.timeEnd();
+    saveClassifier('classifier.json');
+}
+
+//add to test
+//manually add document
+const start = (argv) => {
+    const command = argv[2];
+    switch (command) {
+        case 'new':
+            console.log('in new');
+            prepareDataSet('yelp.txt');
+            break;
+        case 'test':
+            loadClassifier('classifier.json', testCB);
+            break;
+        case 'add':
+            loadClassifier('classifier.json', retrainClassifier)
+            break;
+
+        case 'one':
+            loadClassifier('classifier.json', addSingleDocument(argv[3], argv[4]))
+            break;
+    }
+}
+console.log(start(process.argv));
